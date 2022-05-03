cfb58a35f98a7aa40983e056d9165e4028006076
jobrien <jobrien@bandwidth.com>
2022-05-03 16:22:01 +0100

    Fixing Behaviour, adding tests

--- src/main/java/com/example/consecutivenumbercalculator/GFG.java
+++ src/main/java/com/example/consecutivenumbercalculator/GFG.java
@@ -5,59 +5,44 @@ import java.util.ArrayList;
 
 class GFG {
 
-    static ArrayList<Integer> findLongestConseqSubseq(int[] arr,
-                                                      int n) {
+    public static void main(String[] args) throws Exception {
+        int[] orderedArray = {1, 2, 3, 4, 9, 10, 20,
+                22, 23, 24, 25, 26, 27, 28,
+                39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+                100, 101, 102};
 
-        int ans = 0, count = 0;
         int requestedSize = 8;
 
-        ArrayList<Integer> v = new ArrayList<>();
+        System.out.println(findLongestConsecutiveSubset(orderedArray, requestedSize));
+    }
 
-        // Insert repeated elements
-        // only once in the vector
-        for (int i = 1; i < n; i++) {
-            if (arr[i] != arr[i - 1])
-                v.add(arr[i]);
-        }
+    static ArrayList<Integer> findLongestConsecutiveSubset(int[] queryReturnedFromArray, int sizeRequestedByCustomer) throws Exception {
 
-        // Find the maximum length
-        // by traversing the array
-        for (int i = 0; i < v.size(); i++) {
+        int ans = 0, count = 0;
 
-            // Check if the current element is
-            // equal to previous element +1
-            if (i > 0 && v.get(i) == v.get(i - 1) + 1) {
+        for (int i = 0; i < queryReturnedFromArray.length; i++) {
+            if (i > 0 && queryReturnedFromArray[i] == queryReturnedFromArray[i - 1] + 1) {
                 count++;
             } else {
                 count = 1;
             }
 
-            // Update the maximum
             ans = Math.max(ans, count);
 
-            if (ans >= requestedSize) {
+            if (ans >= sizeRequestedByCustomer) {
                 ArrayList<Integer> consecutiveList = new ArrayList<>();
-                for (int j = 1; j < requestedSize + 1; j++) {
-                    consecutiveList.add(v.get(i - requestedSize + j));
-//                    System.out.println(v.get(i - requestedSize + j));
+                for (int j = 1; j < sizeRequestedByCustomer + 1; j++) {
+                    consecutiveList.add(queryReturnedFromArray[i - sizeRequestedByCustomer + j]);
+                    System.out.println(queryReturnedFromArray[i - sizeRequestedByCustomer + j]);
                 }
                 return consecutiveList;
             }
 
         }
-        return new ArrayList<>();
+        //If no selection is available
+        throw new Exception();
     }
 
-    // Driver code
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 9, 10, 20,
-                22, 23, 24, 25, 26, 27, 28,
-                39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
-                100, 101, 102};
-        int n = arr.length;
-
-        System.out.println(findLongestConseqSubseq(arr, n));
-    }
 }
 
 
---
+++ src/test/java/com/example/consecutivenumbercalculator/GFGTest.java
@@ -0,0 +1,91 @@
+package com.example.consecutivenumbercalculator;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static com.example.consecutivenumbercalculator.GFG.findLongestConsecutiveSubset;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GFGTest {
+
+    @Test
+    void findLongestConsecutiveSubsetTest_8() throws Exception {
+        int[] arr = {1, 2, 3, 4, 9, 10, 20,
+                22, 23, 24, 25, 26, 27, 28,
+                39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+                100, 101, 102};
+        int n = arr.length;
+
+        int requestedSize = 8;
+
+        ArrayList<Integer> actualList = findLongestConsecutiveSubset(arr, requestedSize);
+
+        assertEquals(actualList.get(0), 39);
+        assertEquals(actualList.get(1), 40);
+        assertEquals(actualList.get(2), 41);
+        assertEquals(actualList.get(3), 42);
+        assertEquals(actualList.get(4), 43);
+        assertEquals(actualList.get(5), 44);
+        assertEquals(actualList.get(6), 45);
+        assertEquals(actualList.get(7), 46);
+
+        assertEquals(8, actualList.size());
+    }
+
+    @Test
+    void findLongestConsecutiveSubsetTest_6() throws Exception {
+        int[] arr = {1, 2, 3, 4, 9, 10, 20,
+                22, 23, 24, 25, 26, 27, 28,
+                39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+                100, 101, 102};
+        int n = arr.length;
+
+        int requestedSize = 6;
+
+        ArrayList<Integer> actualList = findLongestConsecutiveSubset(arr, requestedSize);
+
+        assertEquals(actualList.get(0), 22);
+        assertEquals(actualList.get(1), 23);
+        assertEquals(actualList.get(2), 24);
+        assertEquals(actualList.get(3), 25);
+        assertEquals(actualList.get(4), 26);
+        assertEquals(actualList.get(5), 27);
+        assertEquals(6, actualList.size());
+    }
+
+    @Test
+    void findLongestConsecutiveSubsetTest_4() throws Exception {
+        int[] arr = {1, 2, 3, 4, 9, 10, 20,
+                22, 23, 24, 25, 26, 27, 28,
+                39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+                100, 101, 102};
+        int n = arr.length;
+
+        int requestedSize = 4;
+
+        ArrayList<Integer> actualList = findLongestConsecutiveSubset(arr, requestedSize);
+
+        assertEquals(actualList.get(0), 1);
+        assertEquals(actualList.get(1), 2);
+        assertEquals(actualList.get(2), 3);
+        assertEquals(actualList.get(3), 4);
+        assertEquals(4, actualList.size());
+    }
+
+    @Test
+    void findLongestConsecutiveSubsetTest_invalidAmount() throws Exception {
+        int[] arr = {1, 2, 3, 4, 9, 10, 20,
+                22, 23, 24, 25, 26, 27, 28,
+                39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+                100, 101, 102};
+        int n = arr.length;
+
+        int requestedSize = 15;
+
+        ArrayList<Integer> actualList = findLongestConsecutiveSubset(arr, requestedSize);
+
+        assertEquals(15, actualList.size());
+    }
+
+}
